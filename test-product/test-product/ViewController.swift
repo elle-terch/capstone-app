a1ccb5e570fffc0fdb9f77e9858316ce50b350d9
Lindsay Terchin <lterchin@gmail.com>
2019-01-02 15:05:34 -0800

    initial setup of react project

---
+++ test-product/test-product/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test-product
+//
+//  Created by Lindsay Terchin on 1/2/19.
+//  Copyright © 2019 Lindsay Terchin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
